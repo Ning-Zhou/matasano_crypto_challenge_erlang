f93b66cc6472e518a9d381c4abafa44b590ab38e
Ning Zhou <zhou.ning.gd@gmail.com>
2015-02-07 00:55:56 +0100

    s1c1: Give a solution
    Remaining: save the output in a file; run the test in bash shell

---
+++ set1/challenge1/solution.erl
@@ -0,0 +1,12 @@
+-module(solution).
+-export([test_solution/0]).
+
+hex_str_to_base64(Hex_String)->
+    {ok,[Num], _} = io_lib:fread("~16u", Hex_String),
+    A = binary:encode_unsigned(Num, big),
+    base64:encode(A).
+
+test_solution()->
+    Test_String = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+    hex_str_to_base64(Test_String).
+
