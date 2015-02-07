273bbb6d0edf7a3455a2e5f54b68b0262d05925b
Ning Zhou <zhou.ning.gd@gmail.com>
2015-02-07 13:00:48 +0100

    s1c1: Build a shell to run the program, save screen output to a file

--- set1/challenge1/solution.erl
+++ set1/challenge1/solution.erl
@@ -8,5 +8,6 @@ hex_str_to_base64(Hex_String)->
 
 test_solution()->
     Test_String = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
-    hex_str_to_base64(Test_String).
+    Output = hex_str_to_base64(Test_String),
+    io:format("~p~n", [Output]).
 
