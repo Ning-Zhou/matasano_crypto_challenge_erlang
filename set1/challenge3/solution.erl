8f556d0bc19f6e185ee71a25c2b4926ac60973e3
Ning Zhou <zhou.ning.gd@gmail.com>
2015-02-10 23:16:51 +0100

    s1c1: Give a solution

--- set1/challenge3/solution.erl
+++ set1/challenge3/solution.erl
@@ -2,7 +2,7 @@
 -export([test_solution/0, test_decipher/0]).
 
 test_solution()->
-    io:format("~p~n", [text_scoring(<<"pppetaoinshrdlukkk">>)]).
+    io:format("~p~n", [solution()]).
 
 text_scoring(Text_Binary) ->
     text_scoring(Text_Binary, 0, 0).
@@ -16,14 +16,29 @@ text_scoring(<<X:8, Rest_String/binary>>, Num_Scored_Letters, Num_Letters) ->
 	    end,
     text_scoring(Rest_String, Num_Scored_Letters + Point, Num_Letters+1).
 
-%% solution()->
-%%     Encrypted_Text = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
-%%     {ok,[Encrypted_Int], _} = io_lib:fread("~16u", Encrypted_Text).
-%%     Plain_Text = get_plain_text(Encrypted_Int, length(Encrypted_Text)/2).
+solution()->
+    Encrypted_Text = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
+    {ok,[Encrypted_Int], _} = io_lib:fread("~16u", Encrypted_Text),
+    {_Best_Score, Best_Key} = best_guess_key(Encrypted_Int, length(Encrypted_Text) div 2),
+    decipher(Encrypted_Int, Best_Key, length(Encrypted_Text) div 2).
 
-%get_plain_text(Encrypted_Int, Length) ->
-%best_guess_text(Encrypted_Text, Key, {Best_Score, Best_key})->
 
+
+best_guess_key(Encrypted_Int, Length) ->
+    best_guess_key(Encrypted_Int, Length, 0, {0, 0}).
+
+best_guess_key(_Encrypted_Int, _Length, 256, {Best_Score, Best_key})-> 
+    {Best_Score, Best_key};
+best_guess_key(Encrypted_Int, Length, Key, {Best_Score, Best_Key})->
+    Deciphered_By_Key = decipher(Encrypted_Int, Key, Length),
+    Score = text_scoring(Deciphered_By_Key),
+    {Best_Score1, Best_Key1} = 
+	case Score > Best_Score of 
+	    true -> {Score, Key};
+	    false -> {Best_Score, Best_Key}
+	end,
+    best_guess_key(Encrypted_Int, Length, Key+1, {Best_Score1, Best_Key1}).
+    
 
 decipher(Encrypted_Int, Key, Length) ->
     Key_Binary = binary:copy(<<Key:8>>,Length),
