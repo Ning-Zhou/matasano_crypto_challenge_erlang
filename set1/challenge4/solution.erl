8a17a9d02cbd3f2a70bcdfeb2a2fc60786f06551
Ning Zhou <zhou.ning.gd@gmail.com>
2015-03-01 01:17:24 +0100

    s1c4: Refactor the code a bit

--- set1/challenge4/solution.erl
+++ set1/challenge4/solution.erl
@@ -8,7 +8,7 @@ test_solution()->
 solution()->
     Lines = read_lines("4.txt"),
     Evaluations = [decipher(string:strip(X, both, $\n))||X<-Lines],
-    
+
     Scores = [Score || {Score,_,_} <- Evaluations],
     Max_Score = lists:max(Scores),
     io:format("~p~n", [Max_Score]),
@@ -22,7 +22,7 @@ read_lines(Filename) ->
     Lines = read_lines_from(Device),
     file:close(Device),
     Lines.
-    
+
 read_lines_from(FileDevice) ->
         case file:read_line(FileDevice) of
 	    {ok, Data} ->
@@ -33,40 +33,44 @@ read_lines_from(FileDevice) ->
 
 
 
-decipher(Encrypted_Text)->
-    {ok,[Encrypted_Int], _} = io_lib:fread("~16u", Encrypted_Text),
-    {Estimated_Score, Estimated_Key} = best_guess_key(Encrypted_Int, length(Encrypted_Text) div 2),
-    Estimated_String= decipher(Encrypted_Int, Estimated_Key, length(Encrypted_Text) div 2),
+decipher(EncryptedText)->
+    {ok,[EncryptedInt], _} = io_lib:fread("~16u", EncryptedText),
+    EncryptedBits = (length(EncryptedText) div 2) * 8,
+    EncryptedBinary = <<EncryptedInt:EncryptedBits>>,
+    {Estimated_Score, Estimated_Key} = best_guess_key(EncryptedBinary),
+    Estimated_String= decipher(EncryptedBinary, Estimated_Key),
 
     {Estimated_Score, Estimated_Key, Estimated_String}.
 
 
-best_guess_key(Encrypted_Int, Length) ->
-    best_guess_key(Encrypted_Int, Length, 0, {0, 0}).
+best_guess_key(EncryptedBinary) ->
+    best_guess_key(EncryptedBinary, 0, {0, 0}).
 
-best_guess_key(_Encrypted_Int, _Length, 256, {Best_Score, Best_key})-> 
+best_guess_key(_EncryptedBinary, 256, {Best_Score, Best_key})->
     %io:format(io:format("~p~n", [Best_Score])),
     {Best_Score, Best_key};
-best_guess_key(Encrypted_Int, Length, Key, {Best_Score, Best_Key})->
-    Deciphered_By_Key = decipher(Encrypted_Int, Key, Length),
+best_guess_key(EncryptedBinary, Key, {Best_Score, Best_Key})->
+    Deciphered_By_Key = decipher(EncryptedBinary, Key),
     Score = text_scoring(Deciphered_By_Key),
-    {Best_Score1, Best_Key1} = 
-	case Score > Best_Score of 
+    {Best_Score1, Best_Key1} =
+	case Score > Best_Score of
 	    true -> {Score, Key};
 	    false -> {Best_Score, Best_Key}
 	end,
-    best_guess_key(Encrypted_Int, Length, Key+1, {Best_Score1, Best_Key1}).
-    
+    best_guess_key(EncryptedBinary, Key+1, {Best_Score1, Best_Key1}).
 
-decipher(Encrypted_Int, Key, Length) ->
+
+decipher(EncryptedBinary, Key) ->
+    Length = byte_size(EncryptedBinary),
     Key_Binary = binary:copy(<<Key:8>>,Length),
     Size = Length*8,
     << Key_Int: Size >> = Key_Binary,
+    << Encrypted_Int: Size >> = EncryptedBinary,
     Plain_Int = Encrypted_Int bxor Key_Int,
     <<Plain_Int:Size>>.
 
 test_decipher()->
-    Decipher_Binary = decipher(16#ffff, 16#ff, 2),
+    Decipher_Binary = decipher(<<16#ffff:16>>, 16#ff),
     io:format("~p", [Decipher_Binary]).
 
 text_scoring(Text_Binary) ->
