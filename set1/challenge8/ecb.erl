7473776ea0a955f211ed3b6f0152ec9cea5fc47c
Ning Zhou <zhou.ning.gd@gmail.com>
2015-04-08 01:20:19 +0200

    0108: Give solution, iteration 1

--- set1/challenge8/ecb.erl
+++ set1/challenge8/ecb.erl
@@ -1,19 +1,19 @@
 -module(ecb).
 -export([is_ecb_cipher_binary/2]).
 
-is_member(SubBin, Bin) when byte_size(SubBin) > byte_size(Bin) -> false;
+is_member(SubBin, Bin) when bit_size(SubBin) > bit_size(Bin) -> false;
 is_member(SubBin, Bin) ->
     SubSize = byte_size(SubBin),
     <<H:SubSize/binary, _T/bitstring>> = Bin,
     case H == SubBin of
-	true ->
+	true -> print(H),
 	    true;
 	false -> <<_P:4, R/bitstring>> = Bin, is_member(SubBin, R)
     end.
 
 is_ecb_cipher_binary(Bitstring, Ecbsize) when bit_size(Bitstring) < Ecbsize*16 -> false;
 is_ecb_cipher_binary(Bitstring, EcbSize)->
-    print(Bitstring),
+    %% print(Bitstring),
     <<Head:EcbSize/binary, Tail/bitstring>> = Bitstring,
     case is_member(Head, Tail) of 
 	true ->
---
+++ set1/challenge8/solution.erl
@@ -0,0 +1,37 @@
+-module(solution).
+-export([solution/0]).
+
+solution()->
+    Lines = read_lines("8.txt"),
+    [io:format(Line) || Line <- Lines, is_ecb_cipher_text(Line)].
+
+is_ecb_cipher_text(Line)->
+    %% io:format(Line),
+    Int = list_to_integer(Line, 16),
+    Size = length(Line)*4, 
+    Binary = <<Int:Size>>,
+    ecb:is_ecb_cipher_binary(Binary, 16).
+
+read_lines(Filename) ->
+    {ok, Device} = file:open(Filename, [read]),
+    %% io:format("~p~n", [Device]),
+    Lines = read_lines_from(Device),
+    file:close(Device),
+    Lines.
+
+read_lines_from(FileDevice) ->
+    lists:reverse(read_lines_from(FileDevice, [])).
+
+read_lines_from(FileDevice, Accu) ->
+    case file:read_line(FileDevice) of
+	{ok, Data} ->
+	    %% io:format("~p~n", [Data]),
+	    read_lines_from(FileDevice, [string:strip(Data, right, $\n)|Accu]);
+	eof        -> Accu
+    end.
+
+
+
+
+
+
